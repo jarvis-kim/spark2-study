dc6e3e5499bd12928ca38d0dfbe74029b86befe2
jarvis <jarvis88@daumcorp.com>
2017-05-05 15:37:50 +0900

    mapValues, flatMapValues, zip, zipPartitions, groupBy, groupByKey, cogroup

--- src/test/java/kr/co/jarvisk/study/spark/RddTest.java
+++ src/test/java/kr/co/jarvisk/study/spark/RddTest.java
@@ -1,16 +1,18 @@
 package kr.co.jarvisk.study.spark;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.junit.Before;
 import org.junit.Test;
+import scala.Array;
+import scala.Tuple2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -24,6 +26,7 @@ public class RddTest {
         sc = new JavaSparkContext(sparkConf);
     }
 
+    /* --- Map Operation */
 
     @Test
     public void testMap() {
@@ -45,6 +48,9 @@ public class RddTest {
 //        List<String[]> arrayData = rdd1.map(s -> s.split(",")).collect();
 //        arrayData.stream().forEach(array -> System.out.println(array));
     }
+
+
+    /* --- Partition Operation */
 
     /**
      * 파티션 3개 생성.
@@ -77,5 +83,86 @@ public class RddTest {
         }, true);
 
         System.out.println(rdd2.collect());
+    }
+
+
+    /* -- Pair Operation */
+    @Test
+    public void testMapValues() {
+        JavaRDD<String> rdd1 = sc.parallelize(Arrays.asList("a", "b", "c"));
+        JavaPairRDD<String, Integer> rdd2 = rdd1.mapToPair((String t) -> new Tuple2<>(t, 1))
+                .mapValues((Integer v1) -> v1 + 1);
+
+        System.out.println(rdd2.collect());
+    }
+
+    @Test
+    public void testFlatMapValues() {
+        JavaPairRDD<Integer, String> rdd1 = sc.parallelizePairs(Arrays.asList(new Tuple2(1, "a,b"), new Tuple2(2, "a,c"), new Tuple2(3, "d,e")));
+        JavaPairRDD<Integer, String> rdd2 = rdd1.flatMapValues((String s) -> Arrays.asList(s.split(",")));
+        System.out.println(rdd2.collect());
+    }
+
+
+    /* Group Operation */
+
+    @Test
+    public void testZip() {
+        JavaRDD<String> rdd1 = sc.parallelize(Arrays.asList("a", "b", "c"));
+        JavaRDD<Integer> rdd2 = sc.parallelize(Arrays.asList(1, 2, 3));
+
+        JavaPairRDD<String, Integer> rdd3 = rdd1.zip(rdd2);
+        System.out.println(rdd3.collect());
+    }
+
+    @Test
+    public void testZipPartitions() {
+        JavaRDD<String> rdd1 = sc.parallelize(Arrays.asList("a", "b", "c"), 2);
+        JavaRDD<Integer> rdd2 = sc.parallelize(Arrays.asList(1, 2, 3), 2);
+
+        JavaRDD<String> rdd3 = rdd1.zipPartitions(rdd2, (Iterator<String> stringIterator, Iterator<Integer> integerIterator) -> {
+            List<String> list = new ArrayList<>();
+            stringIterator.forEachRemaining(s -> {
+                System.out.println("stringIterator.forEachRemaining : " + s);
+                integerIterator.forEachRemaining(i -> {
+                    System.out.println("integerIterator.forEachRemaining : " + i);
+                    list.add(s + i);
+                });
+            });
+
+            return list.iterator();
+        });
+
+        System.out.println(rdd3.collect());
+    }
+
+
+    @Test
+    public void testGroupBy() {
+        JavaRDD<Integer> rdd1 = sc.parallelize(IntStream.range(1, 11).boxed().collect(Collectors.toList()));
+        JavaPairRDD<String, Iterable<Integer>> rdd2 = rdd1.groupBy(v1 -> v1.intValue() % 2 == 0 ? "even" : "odd");
+        System.out.println(rdd2.collect());
+    }
+
+
+    @Test
+    public void testGroupByKey() {
+        List<Tuple2<String, Integer>> list = Arrays.asList("a", "b", "c", "b", "c").stream().map(s -> new Tuple2<>(s, 1)).collect(Collectors.toList());
+        JavaPairRDD<String, Integer> rdd1 = sc.parallelizePairs(list);
+        JavaPairRDD<String, Iterable<Integer>> rdd2 =  rdd1.groupByKey();
+        System.out.println(rdd2.collect());
+    }
+
+    @Test
+    public void testCogroup() {
+        JavaPairRDD<String, String> rdd1= sc.parallelize(Arrays.asList("k1", "k2", "k1")).zip(sc.parallelize(Arrays.asList("v1", "v2", "v3")));
+        JavaPairRDD<String, String> rdd2 = sc.parallelizePairs(Arrays.asList(new Tuple2<>("k1", "v4")));
+        JavaPairRDD<String, Tuple2<Iterable<String>, Iterable<String>>> rdd3 =  rdd1.cogroup(rdd2);
+
+        System.out.println(rdd3.collect());
+        System.out.println(rdd2.cogroup(rdd1).collect());
+
+        JavaPairRDD<String, String> rdd4 = sc.parallelizePairs(Arrays.asList(new Tuple2<>("k2", "v4")));
+        System.out.println(rdd1.cogroup(rdd2, rdd4).collect());
     }
 }
